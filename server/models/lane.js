6db838979f22cfc29952ac1356a4af70bd453520
Krzysztof Szypulski <krzychu700@gmail.com>
2018-12-06 17:58:07 +0100

    added basic files

---
+++ server/models/lane.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import Note from '../models/note'
+
+const Schema = mongoose.Schema;
+mongoose.plugin(schema => { schema.options.usePushEach = true });
+
+const laneSchema = new Schema({
+  name: { type: 'String', required: true },
+  notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
+  id: { type: 'String', required: true, unique: true },
+});
+
+function populateNotes(next) {
+  this.populate('notes');
+  next();
+}
+
+function deleteNotes(next) {
+  const notes = this.notes;
+  notes.forEach(element => {
+    Note.findByIdAndRemove(element._id).exec()
+  });
+  next();
+}
+
+laneSchema.pre('find', populateNotes);
+laneSchema.pre('findOne', populateNotes);
+laneSchema.pre('remove', deleteNotes);
+
+export default mongoose.model('Lane', laneSchema);
---
+++ server/routes/note.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import * as NoteController from '../controllers/note.controller';
+
+const router = new Router();
+
+// Add a new Note
+router.route('/notes').post(NoteController.addNote);
+
+
+export default router;
